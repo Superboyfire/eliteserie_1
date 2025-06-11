03fda61046710e0a14e8b150587e4aa73dc8574d
Superboyfire <53878280+Superboyfire@users.noreply.github.com>
2025-06-11 19:55:20 +0200

    Update __init__.py

--- model/__init__.py
+++ model/__init__.py
@@ -1,12 +1,1 @@
-import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
-import joblib
 
-from features.build_features import prepare_data
-
-data = pd.read_csv("data/eliteserien_data.csv")
-X, y = prepare_data(data)
-
-model = RandomForestClassifier(n_estimators=100, random_state=42)
-model.fit(X, y)
-joblib.dump(model, "model/rf_model.pkl")
